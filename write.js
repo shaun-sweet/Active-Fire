110111de2c4ec5bdfd65948c449707206e43377f
sweetkeys <shaun@shaunsweet.com>
2016-06-12 00:43:51 -0700

    json read/write working

--- write.js
+++ write.js
@@ -1,6 +1,11 @@
-const fs = require('fs');
+var jsonfile = require('jsonfile')
+var file = './butts/text.json'
+var obj = {name: 'JP'}
+ 
+jsonfile.writeFile(file, obj, function (err) {
+  console.error(err)
+})
 
-fs.rename('./butts/text.txt', '.butts/thiswaschanged.txt', (err) => {
-  if (err) throw err;
-  console.log('successfully renamed stuff');
-});
+jsonfile.readFile(file, function(err, obj) {
+  console.dir(obj)
+})
