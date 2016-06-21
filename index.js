bbf7bc23a8959f30a583c9c68f4a4bf4a9770409
sweetkeys <shaun@shaunsweet.com>
2016-06-20 18:30:06 -0700

    changed runner code for testing

--- index.js
+++ index.js
@@ -5,18 +5,31 @@ var fb = require('./lib/activefire');
 
 var activeFire = new fb.activefire('./ENV/service.json', 'https://active-record.firebaseio.com');
 
-activeFire.newModel('vivianita',
-						{ name: 'string',
-							age: 'number',
-							breed: "string",
-							kittens: 'kitten'
-							})
 
-// activeFire.newEntry('viviand','katty',{
-// 	name: 'flufferss',
-// 	age: 1
-// })
 
+
+
+generateModels(){
+	activeFire.newModel('comments',{
+		user: 'string',
+		body: 'string'
+	})
+
+	activeFire.newModel('users',{
+		username: 'string'
+	})
+}
+
+generateEntries(){
+	activeFire.newEntry('comments', 'comment3', {
+		user: 'vivian',
+		body: 'this is vivians fucking comment'
+	})
+
+	activeFire.newEntry('users','vivian',{
+		username: 'vivian',
+	})
+}
 app.get('/', function (req, res) {
   res.sendfile('index.html');
 });
