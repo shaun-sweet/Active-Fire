625cd71faefededc025711c1917c92a68abaf189
sweetkeys <shaun@shaunsweet.com>
2016-06-20 20:30:18 -0700

    fix bugs before building relationship management

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ var fb = require('./lib/activefire');
 
 var activeFire = new fb.activefire('./ENV/service.json', 'https://active-record.firebaseio.com');
 
-generateModels();
+
+// generateModels();
+generateEntries();
 
 app.get('/', function (req, res) {
   res.sendfile('index.html');
@@ -14,6 +16,13 @@ app.get('/', function (req, res) {
 app.listen(3000, function () {
   console.log('IM LISTENING!!!!!!!!!');
 });
+
+// function seed(){
+// 	return new Promise((resolve,reject)=>{
+// 		generateModels();
+// 		resolve();
+// 	})
+// }
 function generateModels(){
 	activeFire.newModel('comments',{
 		attributes: {
@@ -34,15 +43,15 @@ function generateModels(){
 		}
 	})
 
-}	
+}
 
 function generateEntries(){
-	activeFire.newEntry('commentss', 'comment3', {
+	activeFire.newEntry('comments', {
 		user: 'vivian',
 		body: 'this is vivians fucking comment'
 	})
 
-	activeFire.newEntry('users','vivian',{
+	activeFire.newEntry('users', {
 		username: 'vivian',
 	})
 }
